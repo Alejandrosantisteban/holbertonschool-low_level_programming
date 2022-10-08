0a322a5197a021284a3a83907a34e5163b26ac21
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-08 13:13:09 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
 * print_alphabet - Make the alphabet
 *
 * Return: void
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-		char c;
+     char c;
 
-		for (c = 'a'; c <= 'z'; c++)
-		{
-		    _putchar(c);
-		}
+     for (c = 'a'; c <= 'z'; c++)
+     {
+	 _putchar(c);
+     }
 
-		_putchar('\n');
+     _putchar('\n');
 }
 
