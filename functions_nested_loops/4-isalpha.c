f83c4fb93e10485db96a63c2b2978bd7b380d145
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-08 22:10:30 -0700

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -7,5 +7,11 @@ include "main.h"
  */
 int _isalpha(int c)
 {
-		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A' && (c <= 'Z')))
+
+			return (1);
+
+			else
+
+			return (0);
+			}
