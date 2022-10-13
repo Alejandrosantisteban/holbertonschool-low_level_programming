77876bb1dad627ae4fca477531abccf4083e32b3
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-12 18:58:26 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of the string
+ * @str: string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int size;
+	int half;
+	int x;
+
+	size = 0;
+	while (*(str + size) != '\0')
+		size++;
+	if (size % 2 != 0)
+		half = (size - 1) / 2;
+	else 
+		half - size / 2;
+	for (i = (size - half); x < size; x++)
+		_putchar(str[x]);
+	_putchar(10);
+}
+
