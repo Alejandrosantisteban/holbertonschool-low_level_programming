083832ad0c91a9dc975652ab56d82815dd77c276
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-13 19:24:07 -0700

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings using at most
- *            an inputted number of bytes from src.
- * @dest: The string to be apprended upon.
- * @n: The number of bytes from src to be appended to dest.
- *
- * Return: A pointer to the resulting string dest.
+ * _strcat - this is a funcion strcat
+ * @dest: first param
+ * @src: second param 
+ * Return: a string
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int len = 0, x;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[len])
+		len++;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	for (x = 0; src[x] != 0; x++)
+	{
+		dest[len] = src[x];
+		len += 1;
+	}
+	dest[len] = '\0';
 	return (dest);
-}
