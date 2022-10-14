2dc4c126c8001c11b7792cb56eb2636f50f5c0ae
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-14 09:49:01 -0700

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,7 @@ char *_strcat(char *dest, char *src)
 {
 	int lend = 0, x;
 	int lens = 0;
-	
+
 	while (src[lens])
 		lens++;
 
@@ -22,4 +22,4 @@ char *_strcat(char *dest, char *src)
 		dest[lend + x] = src[x];
 	}
 	return (dest);
-}		
+}
