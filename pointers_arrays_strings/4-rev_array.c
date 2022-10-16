5f82fb59afa437412ab5a1e0e45cf8c38d677614
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-16 11:23:03 -0700

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
- * reverse array - reversing an array
+ * reverse_array - reverses an array of integers
  * @a: array a 
  * @n: an element of an array
  */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
-	a = [1, 2, 3, 4];
-	aux = 1
-	a = [4, 2, 3, 1]
-	p = a;
-	for (i = 0; i < n; i++)
-        p++;
-	for (k = 0; k < n / 2; k++)
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
 	{
-		aux = a [k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a [index];
+		a[ index] = tmp;
 	}
 }
