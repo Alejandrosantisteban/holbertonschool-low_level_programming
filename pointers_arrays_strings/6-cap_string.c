5df2bf1d64bc1217119d9d2d6d5a502062d54e60
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-16 12:17:56 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to be capitalized
+ *
+ * Return: a pointer to be changed string
+ */
+char *cap_string(char *str)
+{
+	int x = 0;
+
+	while (str[x])
+	{
+		while (!(str[x] >= 'a' && str[x] <= 'z'))
+			x++;
+
+		if (str[x - 1] == ' ' ||
+				str[x - 1] == '\t' ||
+				str[x - 1] == '\n' ||
+				str[x - 1] == ',' ||
+				str[x - 1] == ';' ||
+				str[x - 1] == '.' ||
+				str[x - 1] == '!' ||
+				str[x - 1] == '?' ||
+				str[x - 1] == '"' ||
+				str[x - 1] == '(' ||
+				str[x - 1] == ')' ||
+				str[x - 1] == '{' ||
+				str[x - 1] == '}' ||
+				x == 0)
+			str[x] -= 32;
+
+		x++;
+	}
+
+	return (str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,3 +11,4 @@ char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
