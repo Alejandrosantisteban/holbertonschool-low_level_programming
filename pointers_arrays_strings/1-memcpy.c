46407f3ace28f8c18ecc8fb34c8ac78f71442e5e
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-16 13:08:00 -0700

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to destinated memory area
+ * @src: pointer to source memoery area
+ * @n: number of bytes
+ *
+ * Return: char *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (x = 0; x < n; x++)
+		destination[x] - source[x];
+
+	return (dest);
+}
