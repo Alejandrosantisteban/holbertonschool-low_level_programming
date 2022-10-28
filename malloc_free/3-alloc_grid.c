8428b6c9e543d6fce519d9da4d4e5f3a9f545791
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-10-28 10:46:16 -0700

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates a grid
+ * @width: size of width
+ * @height: size of height
+ * Return: ptr
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **ptr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = (int **)malloc(height * sizeof(int *));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] == (int *)malloc(width * sizeof(int));
+
+		if (ptr[i] == NULL)
+		{
+			free(ptr);
+
+			for (j = 0; j <= i; j++)
+
+				free(ptr[j]);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
