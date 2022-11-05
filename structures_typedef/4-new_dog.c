ff5c3dd7212a643bf8382c76d2f13f99d4c5132f
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-11-04 19:47:57 -0700

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -22,38 +22,20 @@ int _strlen(char *str)
 }
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * _strcopy - copies a string pointed to by src
+ * @dest: the buffer storing the string copy
+ * @src: the source string
  *
- * Return: new struct dog
+ * Return: the pointer to dest
  */
-dog_t *new_dog(char *name, float age, char *owner)
+char *_strcopy(char *dest, char *src)
 {
-	dog_t *doggo;
+	int index = 0;
 
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
 
-	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (doggo->name == NULL)
-	{
-		free(doggo);
-		return (NULL);
-	}
-
-	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (doggo->owner == NULL)
-	{
-		free(doggo->name);
-		free(doggo);
-		return (NULL);
-	}
-
-	doggo->name = _strcopy(doggo->name, name);
-	doggo->age = age;
-	doggo->owner = _strcopy(doggo->owner, owner);
-
-	return (doggo);
+	dest[index] = '\0';
+	
+	return (dest);
 }
