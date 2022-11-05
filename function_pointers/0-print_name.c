aea020d058a22270eee153bf9d3f8543ecc25146
Alejandrosantisteban <alejandrosantistebanaguilar@gmail.com>
2022-11-04 20:36:31 -0700

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlb.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printig function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || if)
+		return;
+	f(name);
+}
